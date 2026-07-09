545b8cbf7366551bbdc3234ba590958b6647bd4e
Xuccessor <ameeribro4@gmail.com>
2026-07-09 13:01:06 +0000

    chore(rebrand): rename to PeerX in src/app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'SwapTrade feature slice is running';
+    return 'PeerX feature slice is running';
   }
 }
